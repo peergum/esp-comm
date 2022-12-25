f48ab0bb92d2e4cdcd683153b19026679fe1da23
Phil Hilger <phil@peergum.com>
2022-12-25 13:40:04 -0500

    fixes

--- common.h
+++ common.h
@@ -12,13 +12,8 @@
 #ifndef __COMMON_H
 #define __COMMON_H
 
-#ifndef MIN
-#define MIN(a,b) ((a)<(b)?(a):(b))
-#endif
-
 #define FAT_MOUNT_POINT "/fat"
-#define WIFI_SCAN_LIST_SIZE 16
-#define WIFI_MAXIMUM_RETRY 3
-#define WIFI_MAX_AP_CONNECTIONS 4
+
+#define DEFAULT_SCAN_LIST_SIZE 16
 
 #endif // __COMMON_H
