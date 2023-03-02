b5c9b37f590aed74dee049cdaca76bbcdd6e8206
Phil Hilger <phil@peergum.com>
2023-03-02 12:57:21 -0500

    license fix

--- common.h
+++ common.h
@@ -3,9 +3,25 @@
  * @author Phil Hilger (phil@peergum.com)
  * @brief 
  * @version 0.1
- * @date 2022-05-31
+ * @date 2023-03-02
  * 
- * @copyright Copyright (c) 2022
+ * CAN-talk. A library for microcontrollers that allows decent comms
+ * over a CAN bus.
+ * 
+ * Copyright (C) 2023, PeerGum
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
  * 
  */
 
